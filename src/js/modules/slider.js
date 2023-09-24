49205d057f1863ba0688b6029dd3ab60889dffe5
LDR <89.freedom@gmail.com>
2023-09-24 18:54:23 +0600

    added youtube player opening by btn

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -16,47 +16,52 @@ export default class Slider {
         }
 
         try {
-            this.hanson.style.opacity = '0';
+            this.hanson.style.opacity = "0";
 
             if (n === 3) {
-                this.hanson.classList.add('animated');
+                this.hanson.classList.add("animated");
                 setTimeout(() => {
-                    this.hanson.style.opacity = '1';
-                    this.hanson.classList.add('slideInUp');
+                    this.hanson.style.opacity = "1";
+                    this.hanson.classList.add("slideInUp");
                 }, 3000);
             } else {
-                this.hanson.classList.remove('slideInUp');
+                this.hanson.classList.remove("slideInUp");
             }
-        } catch(e) {
-
+        } catch (e) {
+            /* empty */
         }
 
-        this.slides.forEach(slide => {
-            slide.style.display = 'none';
+        this.slides.forEach((slide) => {
+            slide.style.display = "none";
         });
 
-        this.slides[this.slideIndex - 1].style.display = 'block';
+        this.slides[this.slideIndex - 1].style.display = "block";
     }
 
     plusSlides(n) {
-        this.showSlides(this.slideIndex += n);
+        this.showSlides((this.slideIndex += n));
     }
 
     render() {
         try {
-            this.hanson = document.querySelector('.hanson');
-        } catch(e) {}
+            this.hanson = document.querySelector(".hanson");
+        } catch (e) {
+            /* empty */
+        }
 
-        this.btns.forEach(btn => {
-            btn.addEventListener('click', () => {
+        this.btns.forEach((btn) => {
+            btn.addEventListener("click", () => {
                 this.plusSlides(1);
             });
-            
-            btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.slideIndex = 1;
-                this.showSlides(this.slideIndex);
-            });
+
+            btn.parentNode.previousElementSibling.addEventListener(
+                "click",
+                (e) => {
+                    e.preventDefault();
+                    this.slideIndex = 1;
+                    this.showSlides(this.slideIndex);
+                }
+            );
         });
 
         this.showSlides(this.slideIndex);
